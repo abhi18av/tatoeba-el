9e2147033feaea8e0e94b26e0ec3751912ad27b8
Abhinav Sharma <abhi18av@gmail.com>
2016-11-26 21:31:22 +0530

    unable to do proper parsing - switching to the minibuffer instead

--- minimal-repl-step-1.el
+++ minimal-repl-step-1.el
@@ -13,8 +13,12 @@
 
 ;;;; Mode definitions for interactive mode
 
-(define-derived-mode dun-mode text-mode "lang-repl"
+(define-derived-mode lang-mode text-mode "lang-repl"
   "Major mode for lang-repl.")
+
+
+
+
 
 (defun dun-parse (arg)
   "Function called when return is pressed in interactive mode to parse line."
@@ -24,34 +28,34 @@
         line)
     (end-of-line)
     (if (and (not (= beg (point))) (not (< (point) beg))
-             (string= "=>> " (buffer-substring (- beg 1) beg)))
+             (string= ">" (buffer-substring (- beg 1) beg)))
         (progn
-          (setq line (downcase (buffer-substring beg (point))))
+          (setq line (buffer-substring beg (point)))
           (princ line)
-          (if (eq (dun-vparse dun-ignore dun-verblist line) -1)
-              (dun-mprinc "I don't understand that.\n")))
-    (goto-char (point-max))
-    (dun-mprinc "\n")))
+          (if (equal "a" "a")
+              (princ "You entered \"a\" \n")))
+      (goto-char (point-max))
+      (dun-mprinc "\n")))
   (dun-messages))
 
 (defun dun-messages ()
   (if dun-dead
       (text-mode)
     (if (eq dungeon-mode 'dungeon)
-	(progn
-	  (if (not (= room dun-current-room))
-	      (progn
-		(dun-describe-room dun-current-room)
-		(setq room dun-current-room)))
-	  (dun-fix-screen)
-	  (dun-mprinc "=>> ")))))
+        (progn
+          (if (not (= room dun-current-room))
+              (progn
+                (dun-describe-room dun-current-room)
+                (setq room dun-current-room)))
+          (dun-fix-screen)
+          (dun-mprinc ">")))))
 
-;;;###autoload
-(defun lang-repl ()
-  "Switch to *lang-repl* buffer and start game."
+(defun tatoeba ()
+  "Switch to *tatoeba* buffer and start game."
   (interactive)
-  (switch-to-buffer "*lang-repl*")
+  (switch-to-buffer "*tatoeba*")
   (dun-mode)
   (setq dun-dead nil)
   (setq room 0)
   (dun-messages))
+
