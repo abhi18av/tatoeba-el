0af26d07bd600fc28455a012941962b6f64c9ff7
Abhinav Sharma <abhi18av@gmail.com>
2016-12-02 14:38:38 +0530

    making progress with varied responses in the simple-repl

--- simple-repl.el
+++ simple-repl.el
@@ -2,6 +2,7 @@
 (defvar simple-repl--*print-space*)
 (defvar simple-repl--*print-upcase*)
 (defvar simple-repl--lincount)
+(defvar simple-repl-found)
 
 (defmacro simple-repl$ (what)
   "Quoted arg form of simple-repl-$."
@@ -83,8 +84,13 @@ Otherwise call the Doctor to parse preceding sentence."
 ;    (memq (second simple-repl-sent) simple-repl--abusewords))
 
 
-    ((and (eq (car simple-repl-sent) 'you)
-          (memq (cadr simple-repl-sent) simple-repl--abusewords)))
+
+
+    ((and (eq (first simple-repl-sent) 'you)
+          (memq (cadr simple-repl-sent) simple-repl--abusewords))
+     (setq simple-repl-found (second simple-repl-sent))
+     (simple-repl-type (simple-repl$ simple-repl--abuselst)))
+
 
      (t
       (insert "the default dialogue from the doctor"))))
@@ -94,6 +100,7 @@ Otherwise call the Doctor to parse preceding sentence."
 
  (defun make-simple-repl-variables ()
    (set (make-local-variable 'simple-repl-sent) nil)
+   (set (make-local-variable 'doctor-found) nil)
     (set (make-local-variable 'simple-repl--lincount) 0)
      (set (make-local-variable 'simple-repl--abusewords)
           '(boring bozo clown clumsy cretin dumb dummy
@@ -155,3 +162,100 @@ Otherwise call the Doctor to parse preceding sentence."
    (append
     (if (and x (atom x)) (list x) x)
     (if (and y (atom y)) (list y) y)))
+
+
+
+ ;;;;;;;;;; 
+ ;; STUDY CAREFULLY
+
+ ;; (defun doctor-doc ()
+ ;;  (cond
+
+ ;;   ((equal doctor-sent '(foo))
+ ;;    (doctor-type '(bar! (doc$ doctor--please) (doc$ doctor--continue) \.)))
+
+
+ ;;   ((member doctor-sent doctor--howareyoulst)
+ ;;    (doctor-type '(i\'m ok \.  (doc$ doctor--describe) yourself \.)))
+
+
+ ;;   ((or (member doctor-sent '((good bye) (see you later) (i quit) (so long)
+ ;;            (go away) (get lost)))
+ ;;  (memq (car doctor-sent)
+ ;;        '(bye halt break quit done exit goodbye
+ ;;        bye\, stop pause goodbye\, stop pause)))
+ ;;    (doctor-type (doc$ doctor--bye)))
+
+
+
+ ;;   ((and (eq (car doctor-sent) 'you)
+ ;;   (memq (cadr doctor-sent) doctor--abusewords))
+ ;;    (setq doctor-found (cadr doctor-sent))
+ ;;    (doctor-type (doc$ doctor--abuselst)))
+
+
+ ;;   ((eq (car doctor-sent) 'whatmeans)
+ ;;    (doctor-def (cadr doctor-sent)))
+
+
+ ;;   ((equal doctor-sent '(parse))
+ ;;    (doctor-type (list  'subj '= doctor-subj ",  "
+ ;;      'verb '= doctor-verb "\n"
+ ;;      'object 'phrase '= doctor-obj ","
+ ;;      'noun 'form '=  doctor-object "\n"
+ ;;      'current 'keyword 'is doctor-found
+ ;;      ", "
+ ;;      'most 'recent 'possessive
+ ;;      'is doctor-owner "\n"
+ ;;      'sentence 'used 'was
+ ;;      "..."
+ ;;      '(doc// doctor--bak))))
+
+
+ ;;   ((memq (car doctor-sent) '(are is do has have how when where who why))
+ ;;    (doctor-type (doc$ doctor--qlist)))
+ ;;   ;;   ((eq (car sent) 'forget)
+ ;;   ;;    (set (cadr sent) nil)
+ ;;   ;;    (doctor-type '((doc$ doctor--isee) (doc$ doctor--please)
+ ;;   ;;     (doc$ doctor--continue)\.)))
+
+
+ ;;   (t
+ ;;    (if (doctor-defq doctor-sent) (doctor-define doctor-sent doctor-found))
+
+ ;;    (if (> (length doctor-sent) 12)
+ ;;  (setq doctor-sent (doctor-shorten doctor-sent)))
+
+
+ ;;    (setq doctor-sent (doctor-correct-spelling
+ ;;           (doctor-replace doctor-sent doctor--replist)))
+
+
+ ;;    (cond ((and (not (memq 'me doctor-sent)) (not (memq 'i doctor-sent))
+ ;;    (memq 'am doctor-sent))
+ ;;     (setq doctor-sent (doctor-replace doctor-sent '((am . (are)))))))
+
+ ;;    (cond ((equal (car doctor-sent) 'yow) (doctor-zippy))
+ ;;    ((< (length doctor-sent) 2)
+
+ ;;     (cond ((eq (doctor-meaning (car doctor-sent)) 'howdy)
+ ;;      (doctor-howdy))
+ ;;     (t (doctor-short))))
+
+ ;;    (t
+ ;;     (if (memq 'am doctor-sent)
+ ;;         (setq doctor-sent (doctor-replace doctor-sent '((me . (i))))))
+ ;;     (setq doctor-sent (doctor-fixup doctor-sent))
+ ;;     (if (and (eq (car doctor-sent) 'do) (eq (cadr doctor-sent) 'not))
+ ;;         (cond ((zerop (random 3))
+ ;;          (doctor-type '(are you (doc$ doctor--afraidof) that \?)))
+ ;;         ((zerop (random 2))
+ ;;          (doctor-type '(don\'t tell me what to do \. i am the
+ ;;              doctor here!))
+ ;;          (doctor-rthing))
+ ;;         (t
+ ;;          (doctor-type '((doc$ doctor--whysay) that i shouldn\'t
+ ;;             (cddr doctor-sent)
+ ;;             \?))))
+ ;;       (doctor-go (doctor-wherego doctor-sent))))))))
+
