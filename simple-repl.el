9024788223dd32557974dea17a15edc2e19fc922
Abhinav Sharma <abhi18av@gmail.com>
2016-12-07 10:52:59 +0530

    removed all comments

--- simple-repl.el
+++ simple-repl.el
@@ -18,13 +18,13 @@
   `(simple-repl-$ ',what))
 
 (defun simple-repl-$ (what)
-  "Return the car of a list, rotating the list e=======
-ach time."
   (let* ((vv (symbol-value what))
          (first (car vv))
-         (ww (append (cdr vv) (list first))))
+         (ww (append (cdr vv) (list firs
+  (cond
+t))))
     (set what ww)
-    first))
+    first)))
 
 (defvar simple-repl-mode-map
   (let ((map (make-sparse-keymap)))
@@ -33,12 +33,7 @@ ach time."
     map))
 
 (define-derived-mode simple-repl-mode text-mode "simple-repl"
-  "Major mode for running the Doctor (Eliza) program.
-Like Text mode with Auto Fill mode
-except that RET when point is after a newline, or LFD at any time,
-reads the sentence before point, and prints the Doctor's answer."
-  (make-simple-repl-variables)
- ; (setq font-lock-mode nil)
+ (make-simple-repl-variables)
   (insert "In the beginning was the word ...")
   (insert "\n"))
 
@@ -46,18 +41,13 @@ reads the sentence before point, and prints the Doctor's answer."
                              (font-lock-mode -1)) 'append)
 
 
-;; Define equivalence classes of words that get treated alike.
-
 ;;;###autoload
 (defun simple-repl ()
-  "Switch to *doctor* buffer and start giving psychotherapy."
   (interactive)
   (switch-to-buffer "*simple-repl*")
   (simple-repl-mode))
 
 (defun simple-repl-ret-or-read (arg)
-  "Insert a newline if preceding character is not a newline.
-Otherwise call the Doctor to parse preceding sentence."
   (interactive "*p")
   (if (= (preceding-char) ?\n)
       (simple-repl-read-print)
@@ -66,42 +56,14 @@ Otherwise call the Doctor to parse preceding sentence."
 (defun simple-repl-read-print ()
   "Top level loop."
   (interactive)
-;; change the input method
-;;  (set-input-method "latin-alt-postfix")
-
-;;  (toggle-input-method)
-
-
 
   (setq simple-repl-sent (simple-repl-readin))
-  (insert "\n@ ")
+  (insert "\n>>")
   (setq simple-repl--lincount (1+ simple-repl--lincount))
   (simple-repl-doc)
-  (insert "\n=> ")
-
-;;  ;; Reset the method
-;;  (set-input-method "utf-8")
-;;  (toggle-input-method)
+  (insert "\n@")
 
   )
-
-;; (defun simple-repl-readin ()
-;;   "Read a sentence.  Return it as a list of words."
-;;   (let (sentence)
-;;     (backward-sentence 1)
-;;     (while (not (eobp))
-;;       (setq sentence (append sentence (list (simple-repl-read-token)))))
-;;     sentence))
-
-;; (defun simple-repl-read-token ()
-;;   "Read one word from buffer."
-;;   (prog1 (intern (downcase (buffer-substring (point)
-;; 					     (progn
-;; 					       (forward-word 1)
-;; 					       (point)))))
-;;     (re-search-forward "\\Sw*")))
-;; Main processing function for sentences that have been read.
-
 
 
 (defvar sentence nil)
@@ -111,14 +73,6 @@ Otherwise call the Doctor to parse preceding sentence."
 
 (defun simple-repl-readin ()
   (setq sentence (buffer-substring-no-properties (line-beginning-position) (line-end-position)))
-
-;  sentence
-
-
-
-; (setq sentence-word-list
-;      (-map (lambda (x) ( make-symbol x)) (s-split-words sentence)))
-
   (setq sentence-word-list
         (s-split " " sentence))
 
