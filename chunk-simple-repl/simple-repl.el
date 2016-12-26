2c15ee6fbcd2ad35f5888ff68e0604a629e11e02
Abhinav Sharma <abhi18av@gmail.com>
2016-12-26 11:33:59 +0530

    trying to port the entire response section

--- chunk-simple-repl/simple-repl.el
+++ chunk-simple-repl/simple-repl.el
@@ -130,29 +130,5 @@
 
 
 
-(defun simple-repl-response ()
-  (cond
 
-   ;; if the sentence contains only ONE word and the word is in the response list
-
-   (
-    (ht-contains-p response-list-ht (second (s-split " " sentence)))
-     (insert  (lax-plist-get response-list
-                            (second (s-split " " sentence)))))
-
-
-   ((-contains-p (s-split " " sentence) "are")
-    (insert (propertize "rrr" 'face '(:foreground "red"))))
-
-    ;; default case for cond
-    (t (insert (propertize "hmm" 'face '(:foreground "skyblue")))))
-
-
-
-;  (insert (number-to-string (length (s-split " " sentence))))
-
-  (insert "\n")
-
-  (response-message-box )
-  )
-
+(simple-repl-response )
